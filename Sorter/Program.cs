87b2c854c756104637fb28a8eab2f7602004e5cb
occo-git <129328663+occo-git@users.noreply.github.com>
2025-06-03 06:52:48 +0400

    sort 1 file

--- Sorter/Program.cs
+++ Sorter/Program.cs
@@ -13,8 +13,8 @@ var summary2 = BenchmarkRunner.Run<FileProcessingBenchmark>();
 var sw = Stopwatch.StartNew();
 
 var sorter = new MySorter();
-//await sorter.Sort("d:/_data1", 1024 * 1024 * 1024); // 1 GB file
-await sorter.SotrFiles("d:/multi_data", 5, 100 * 1024 * 1024); // 5 files of 100 MB each
+await sorter.Sort("d:/_data1", 1024 * 1024 * 1024); // 1 GB file
+//await sorter.SotrFiles("d:/multi_data", 5, 100 * 1024 * 1024); // 5 files of 100 MB each
 
 sw.Stop();
 Console.WriteLine($"Elapsed time: {sw.ElapsedMilliseconds} ms");
